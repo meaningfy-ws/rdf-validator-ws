92a568975b499be135d66522a83b774a45e601c3
Mihai <coslet.mihai@gmail.com>
2020-09-24 20:06:36 +0300

    Prepare for RDFUnit wrapper

--- validator/entrypoints/api/handlers.py
+++ validator/entrypoints/api/handlers.py
@@ -6,10 +6,24 @@
 # Email: coslet.mihai@gmail.com
 
 """
-Module description
-
+OpenAPI mehod handlers.
 """
+from werkzeug.datastructures import FileStorage
 
 
-def validate_endpoint(dataset_uri, endpoint_url, graphs, schema_files):
-    return {'hello': 'maaaaan'}
+def validate_endpoint(body, schema_files: FileStorage):
+    """
+
+    :param body:
+    :param schema_files:
+    :return:
+    """
+    dataset_uri = body.get('dataset_uri')
+    endpoint_url = body.get('endpoint_url')
+    graphs = body.get('graphs')
+
+    # validate values
+    # see how to work around the one file bug
+    # use RDFUnit wrapper
+
+    return "Success", 200
