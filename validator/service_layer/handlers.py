e0c5c9da088d54b0dbf47ffe3062199fc1783f84
Laurentiu Mandru <mclaurentiu79@gmail.com>
2020-10-08 20:28:46 +0300

    Added RDFUnit delay parameter

--- validator/service_layer/handlers.py
+++ validator/service_layer/handlers.py
@@ -8,13 +8,14 @@
 """ """
 import json
 import logging
+import os
 from distutils.dir_util import copy_tree
 from pathlib import Path
 from typing import List, Union
 from urllib.parse import urlparse
 
 from eds4jinja2.builders.report_builder import ReportBuilder
-
+from dotenv import load_dotenv
 from validator.adapters.validator_wrapper import AbstractValidatorWrapper, RDFUnitWrapper
 
 logger = logging.getLogger(__name__)
@@ -108,9 +109,22 @@ def run_sparql_endpoint_validator(sparql_endpoint_url: str, graphs_uris: List[st
 
     Please see https://github.com/AKSW/RDFUnit/wiki/CLI for a comprehensive description of the parameters
     """
+
+    load_dotenv()
+    try:
+        rdfunit_delay_ms = int(os.environ.get("RDFUNIT_DELAY_MS"), 10)
+    except ValueError:
+        logger.error("Invalid value for RDFUNIT_DELAY_MS: " + \
+                     str(os.environ.get("RDFUNIT_DELAY_MS")) + \
+                     ". Defaulting to 1 ms")
+        rdfunit_delay_ms = 1
+
+
     logger.info("RDFUnitWrapper starting ...")
     validator_wrapper: AbstractValidatorWrapper
     validator_wrapper = RDFUnitWrapper("java")
+
+    sparql_endpoint_url = sparql_endpoint_url.strip()
 
     if graphs_uris is None or len(graphs_uris) == 0:
         graph_param = ""
@@ -123,6 +137,7 @@ def run_sparql_endpoint_validator(sparql_endpoint_url: str, graphs_uris: List[st
                                                       graph_param,
                                                       "-s", ", ".join([schema for schema in schemas]),
                                                       "-r", 'shacl',
+                                                      "-C", "-T", "0", "-D", str(rdfunit_delay_ms),
                                                       "-o", 'html,ttl',
                                                       "-f", str(output))
     logger.info("RDFUnitWrapper finished with output:\n" + cli_output)
